7a7d246ad17ad4ee5e339601b2ed247ccd89d94b
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-17 19:34:05 +0530

    starting  : migrations

--- Startup.cs
+++ Startup.cs
@@ -4,26 +4,26 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace CRUD_Operations
 {
-	public class Startup
-	{
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.AddMvc();
-		}
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-			app.UseStaticFiles();
-			app.UseMvc(routes =>
-			{
-				routes.MapRoute(
-					name: "default",
-					template: "{controller=Home}/{action=Index}/{id?}");
-			});
-		}
-	}
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(x =>
+            {
+                x.MapControllerRoute("Default", pattern: "{controller=Home}/{action=Index}");
+            });
+
+        }
+    }
 }
